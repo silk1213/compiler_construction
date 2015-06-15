e7df3b7e86646b1211681623c340dd4c9be1cdff
silk1213 <lasse.knudsen@gmx.de>
2015-06-15 14:18:04 +0200

    bal

---
+++ interpreter/test/test4.c
@@ -0,0 +1,11 @@
+int main () {
+	int a = 1;
+
+	if (a<1) {
+		a++;
+	} else {
+		a--;
+	}
+	
+	return a;
+}
---
+++ interpreter/test/test5.c
@@ -0,0 +1,12 @@
+int test (int a) {
+	int b = a*a;
+	return b;
+}
+
+
+int main () {
+	int b = 2;
+	int a = test(b);
+	
+	return a;
+}
